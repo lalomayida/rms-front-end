4a8e6dd36864eaad9d07d7ff6d0646524cd68680
lalomayida <lalomayida@gmail.com>
2020-05-01 17:25:28 -0500

    Setting up base application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { NbLayoutModule, NbSidebarModule, NbButtonModule, NbThemeModule, NbThemeService, NbSidebarService } from '@nebular/theme';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbLayoutModule,
+    NbThemeModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbEvaIconsModule
   ],
-  providers: [],
+  providers: [NbSidebarService, NbThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
