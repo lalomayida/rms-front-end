899d4d9bd3f42867ec908478f81bf88e50681a3b
lalomayida <lalomayida@gmail.com>
2020-05-13 01:21:15 -0500

    first deploy

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
 
-// Serve only the static files form the dist directory
+app.use(forceSSL());
+
 app.use(express.static(__dirname + '/dist/rms-front-end'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/rms-front-end/index.html'));
-});
-
-// Start the app by listening on the default H
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/rms-front-end/index.html'));
+  });
+  
 app.listen(process.env.PORT || 8080);
