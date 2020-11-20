b4d97e58ab200228f9d24224fac8c2d6e90a377c
lalomayida <lalomayida@gmail.com>
2020-11-19 18:29:47 -0600

    Other some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guard/auth-guard.service';
 
 const routes: Routes = [
-  {
-    path: 'pages',
-    canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/pages.module')
-      .then(m => m.PagesModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'pages/requisition-dashboard'
-  },
+
 ];
 
 @NgModule({
